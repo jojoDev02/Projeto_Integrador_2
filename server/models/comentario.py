41ab378dfecdf7ac78f5a7dbe39511fc103fae7c
Jordana Cavalcante <jordana-cavalcante@outlook.com>
2023-11-26 19:14:25 -0300

    feat: criando repositories

--- server/models/comentario.py
+++ server/models/comentario.py
@@ -1,3 +1,4 @@
+from typing import Any
 from database import Base
 from sqlalchemy import String, Integer, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
@@ -8,6 +9,16 @@ class Comentario(Base):
     __tablename__ = 'Comentario'
 
     id = Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    conteudo = mapped_column(String(255), nullable=True)
+    texto = mapped_column(String(255), nullable=False)
     publicacao_id = Mapped[int] = mapped_column(ForeignKey("Publicacao.id"))
     publicacao = Mapped["Publicacao"] = relationship(back_populates="comentarios")
+
+    def __init__(self, texto, publicacao_id) -> None:
+        self.texto = texto
+        self.publicacao_id = publicacao_id
+
+    def to_dict(self):
+        return{
+            "id" : self.id,
+            "texto" : self.texto
+        }
---
+++ server/repositories/publicacao_repository.py
@@ -0,0 +1,39 @@
+from models.publicacao import Publicacao
+from database import db_session
+from sqlalchemy.orm.exc import NoResultFound
+class PublicacaoRepository:
+
+    def fetch_by_id(self, id): 
+        return db_session.query(Publicacao).filter(Publicacao.id == id).first()
+
+    def create(self, data):
+
+        usuario_id = data["usuario_id"]
+        conteudo = data["conteudo"]
+        publicacao = Publicacao(conteudo)
+
+        db_session.add(publicacao)
+        db_session.commit()
+
+
+    def update(self, id, novo_conteudo):
+
+        publicacao = self.fetch_by_id(id)
+
+        if publicacao:
+            publicacao.conteudo = novo_conteudo
+            db_session.commit()
+        else:
+            raise NoResultFound("Publicação não encontrada.")
+
+    def delete(self, id): 
+        publicacao = self.fetch_by_id(id)
+        if publicacao:
+            db_session.delete(publicacao)
+            db_session.commit()
+        else:
+            raise NoResultFound("Publicação não encontrada.")
+        
+
+
+    

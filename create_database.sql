65617cddb0af732a38f7c19c6f9bf3d0541424a9
joao coura <joaovitorsouzacoura@gmai.com>
2023-12-04 20:01:09 -0300

    feat: removendo relacao Comunidade_Pulicacao.

--- create_database.sql
+++ create_database.sql
@@ -44,13 +44,6 @@ CREATE TABLE Publicacao (
     curtidas INT NOT NULL
 );
 
--- Tabela Comunidade_Publicacao
-CREATE TABLE Comunidade_Publicacao (
-	publicacaoId INT,
-    comunidadeId INT,
-    PRIMARY KEY (publicacaoId, comunidadeId)
-);
-
 -- Tabela Evento
 CREATE TABLE Evento (
     eventoId INT AUTO_INCREMENT PRIMARY KEY,
@@ -136,14 +129,6 @@ FOREIGN KEY (usuarioId) REFERENCES Usuario(usuarioId);
 ALTER TABLE Publicacao
 ADD comunidadeId INT,
 ADD CONSTRAINT fk_comunidade_pertencente
-FOREIGN KEY (comunidadeId) REFERENCES Comunidade(comunidadeId);
-
-ALTER TABLE Comunidade_Publicacao
-ADD CONSTRAINT fk_publicacao
-FOREIGN KEY (publicacaoId) REFERENCES Publicacao(publicacaoId);
-
-ALTER TABLE Comunidade_Publicacao
-ADD CONSTRAINT fk_comunidade
 FOREIGN KEY (comunidadeId) REFERENCES Comunidade(comunidadeId);
 
 ALTER TABLE Comunidade_Usuario
